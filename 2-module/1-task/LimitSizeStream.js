70fbcd1a6df426ff897fc773c49c4902ce9d24a3
vladimir.slukhov <Vladimir.slukhov@smartbics.com>
2019-06-18 23:39:04 +0300

    add solution for 2-module 1-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,10 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
+    const chunkLength = chunk.length;
 
+    if (this.limit - chunkLength >= 0) {
+      callback(null, chunk);
+      this.limit -= chunkLength;
+    } else {
+      this.destroy(new LimitExceededError());
+    }
   }
 }
 
