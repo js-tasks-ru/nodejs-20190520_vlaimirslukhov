ae7010b9a9ceb9edc4a34f314b7970c8e47149b1
vladimir.slukhov <Vladimir.slukhov@smartbics.com>
2019-06-23 13:50:04 +0300

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,23 +1,54 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
+  const sendResponse = (code = 200, message = '') => {
+    res.statusCode = code;
+    res.end(message);
+  };
+
   const pathname = url.parse(req.url).pathname.slice(1);
+
+  if (pathname.includes('/') || pathname.includes('..')) {
+    sendResponse(400, 'Nested paths are not allowed');
+  }
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  const removeFile = () => {
+    fs.unlink(filepath, (error) => {
+      if (!error) {
+        sendResponse(200);
+        return;
+      }
+
+      const {code} = error;
+
+      if (code === 'ENOENT') {
+        sendResponse(404, 'File not found');
+      }
+
+      sendResponse(500);
+    });
+  };
+
   switch (req.method) {
     case 'DELETE':
-
+      removeFile();
       break;
 
     default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+      sendResponse(501, 'Not implemented');
   }
+
+  res.on('close', () => {
+    if (res.finished) return;
+    stream.destroy();
+  });
 });
 
 module.exports = server;
